cb47b0b3d72191946fe16756488119f1ef7c7f63
amitjslearn <amitjslearn@gmail.com>
2019-10-29 20:59:54 +0530

    not done yet

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 setup(name='wordipy',
       version='0.1',
+      description='wordipy is dummy  package which can be used in preprocessing for NLP',
       py_modules=['wordipy'],
+      #package_dir = {'': 'lib'}
       )
